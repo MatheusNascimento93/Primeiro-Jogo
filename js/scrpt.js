976662b110e63f0f9f930127d0399681139bca4c
MatheusNascimento93 <theu_1993@hotmail.com>
2021-05-17 23:21:49 -0300

    Jogo da cobrinha completo

--- js/scrpt.js
+++ js/scrpt.js
@@ -32,10 +32,10 @@ function drawfood() {
 document.addEventListener('keydown', update);
 
 function update(event) {
-    if (event.keyCode == 37 && direction != "right") direction = "left";
-    if (event.keyCode == 38 && direction != "down") direction = "up";
-    if (event.keyCode == 39 && direction != "left") direction = "right";
-    if (event.keyCode == 40 && direction != "up") direction = "down";
+    if (event.keyCode == 37 && direction != 'right') direction = 'left';
+    if (event.keyCode == 38 && direction != 'down') direction = 'up';
+    if (event.keyCode == 39 && direction != 'left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
 function iniciarJogo() {
@@ -45,6 +45,13 @@ function iniciarJogo() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y > 0 && direction == "up") snake[0].y = 16 * box;
 
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawfood();
@@ -52,10 +59,18 @@ function iniciarJogo() {
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if (direction == "right") snake += box;
-    if (direction == "left") snake -= box;
-    if (direction == "up") snake -= box;
-    if (direction == "down") snake += box;
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    }
+    else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     snake.pop();
 
